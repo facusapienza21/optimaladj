241d57b38926056767a19222a606dfa7c5dfdc68
ezequiel <ezequiels.90@gmail.com>
2021-02-06 12:19:12 +0100

    fix setup file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="optimaladj-facusapienza21",
+    name="optimaladj",
     version="0.0.1",
     author="Facundo Sapienza, Ezequiel Smucler",
     author_email="fsapienza@berkeley.edu",
