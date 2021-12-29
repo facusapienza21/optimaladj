000a888028c8eb355e9db9a9af75684ab33be8e8
Ezequiel Smucler <ezequiel.smucler@glovoapp.com>
2021-12-29 18:40:55 +0100

    final pieces

--- tests/examples.py
+++ tests/examples.py
@@ -2,149 +2,216 @@ from optimaladj.CausalGraph import CausalGraph
 
 
 class CausalGraphExample:
-    def __init__(self, G, treatment, outcome, L, N):
+    def __init__(self, G, treatment, outcome, L, N, costs=None):
         self.G = G
         self.treatment = treatment
         self.outcome = outcome
         self.L = L
         self.N = N
+        self.costs = costs
 
 
 EXAMPLES = []
 OPTIMALS = []
 OPTIMALS_MINIMAL = []
 OPTIMALS_MINIMUM = []
+OPTIMALS_MINCOST = []
 
-# Figure 1 of the paper
+# Figure 1 of the Biometrika paper
 
 G_epi = CausalGraph()
-G_epi.add_edges_from([('coach', 'team motivation'),
-                  ('coach', 'fitness'),
-                  ('fitness', 'pre-game prop'),
-                  ('fitness', 'neuromusc fatigue'),
-                  ('team motivation', 'warm-up'),
-                  ('team motivation', 'previous injury'),
-                  ('pre-game prop', 'warm-up'),
-                  ('warm-up', 'intra-game prop'),
-                  ('contact sport', 'previous injury'),
-                  ('contact sport', 'intra-game prop'),
-                  ('intra-game prop', 'injury'),
-                  ('genetics', 'fitness'),
-                  ('genetics', 'neuromusc fatigue'),
-                  ('genetics', 'tissue disorder'),
-                  ('tissue disorder', 'neuromusc fatigue'),
-                  ('tissue disorder', 'tissue weakness'),
-                  ('neuromusc fatigue', 'intra-game prop'),
-                  ('neuromusc fatigue', 'injury'),
-                  ('tissue weakness', 'injury')])
+G_epi.add_edges_from(
+    [
+        ("coach", "team motivation"),
+        ("coach", "fitness"),
+        ("fitness", "pre-game prop"),
+        ("fitness", "neuromusc fatigue"),
+        ("team motivation", "warm-up"),
+        ("team motivation", "previous injury"),
+        ("pre-game prop", "warm-up"),
+        ("warm-up", "intra-game prop"),
+        ("contact sport", "previous injury"),
+        ("contact sport", "intra-game prop"),
+        ("intra-game prop", "injury"),
+        ("genetics", "fitness"),
+        ("genetics", "neuromusc fatigue"),
+        ("genetics", "tissue disorder"),
+        ("tissue disorder", "neuromusc fatigue"),
+        ("tissue disorder", "tissue weakness"),
+        ("neuromusc fatigue", "intra-game prop"),
+        ("neuromusc fatigue", "injury"),
+        ("tissue weakness", "injury"),
+    ]
+)
 
-L_epi = ['team motivation', 'previous injury']
-N_epi = ['team motivation', 'previous injury', 'warm-up', 'coach', 'fitness',
-     'contact sport', 'neuromusc fatigue', 'tissue disorder', 'injury']
-treatment_epi = 'warm-up'
-outcome_epi = 'injury'
+L_epi = ["team motivation", "previous injury"]
+N_epi = [
+    "team motivation",
+    "previous injury",
+    "warm-up",
+    "coach",
+    "fitness",
+    "contact sport",
+    "neuromusc fatigue",
+    "tissue disorder",
+    "injury",
+]
+treatment_epi = "warm-up"
+outcome_epi = "injury"
 
 EXAMPLES.append(CausalGraphExample(G_epi, treatment_epi, outcome_epi, L_epi, N_epi))
 
-OPTIMALS.append(set(['team motivation', 'previous injury', 'contact sport',
-                     'tissue disorder', 'neuromusc fatigue']))
-OPTIMALS_MINIMAL.append(set(['team motivation', 'previous injury', 'tissue disorder', 'neuromusc fatigue']))
-OPTIMALS_MINIMUM.append(set(['team motivation', 'previous injury', 'fitness']))
+OPTIMALS.append(
+    set(
+        [
+            "team motivation",
+            "previous injury",
+            "contact sport",
+            "tissue disorder",
+            "neuromusc fatigue",
+        ]
+    )
+)
+OPTIMALS_MINIMAL.append(
+    set(["team motivation", "previous injury", "tissue disorder", "neuromusc fatigue"])
+)
+OPTIMALS_MINIMUM.append(set(["team motivation", "previous injury", "fitness"]))
 
 
-# Figure 3 of the paper
+# Figure 3 of the Biometrika paper
 
 G_1 = CausalGraph()
-G_1.add_edges_from([('A', 'M'),
-                    ('T', 'A'),
-                    ('T', 'F'),
-                    ('F', 'A'),
-                    ('U', 'F'),
-                    ('U', 'Y'),
-                    ('M', 'Y')])
-L_1 = ['T']
-N_1 = ['A', 'Y', 'M', 'T', 'F']
-treatment_1 = 'A'
-outcome_1 = 'Y'
+G_1.add_edges_from(
+    [("A", "M"), ("T", "A"), ("T", "F"), ("F", "A"), ("U", "F"), ("U", "Y"), ("M", "Y")]
+)
+L_1 = ["T"]
+N_1 = ["A", "Y", "M", "T", "F"]
+treatment_1 = "A"
+outcome_1 = "Y"
 
 EXAMPLES.append(CausalGraphExample(G_1, treatment_1, outcome_1, L_1, N_1))
 
-OPTIMALS.append(set(['T', 'F']))
-OPTIMALS_MINIMAL.append(set(['T', 'F']))
-OPTIMALS_MINIMUM.append(set(['T', 'F']))
+OPTIMALS.append(set(["T", "F"]))
+OPTIMALS_MINIMAL.append(set(["T", "F"]))
+OPTIMALS_MINIMUM.append(set(["T", "F"]))
 
-# Figure 4 of the paper
+# Figure 4 of the Biometrika paper
 
 G_2 = CausalGraph()
-G_2.add_edges_from([('A', 'Y'),
-                    ('T', 'W1'),
-                    ('T', 'Y'),
-                    ('W1', 'A'),
-                    ('W2', 'W1'),
-                    ('W2', 'Y'),
-                    ('W3', 'W1'),
-                    ('W3', 'Y'),
-                    ('W4', 'Y')])
-L_2 = ['T']
-N_2 = ['A', 'Y', 'T', 'W1', 'W2', 'W3', 'W4']
-treatment_2 = 'A'
-outcome_2 = 'Y'
+G_2.add_edges_from(
+    [
+        ("A", "Y"),
+        ("T", "W1"),
+        ("T", "Y"),
+        ("W1", "A"),
+        ("W2", "W1"),
+        ("W2", "Y"),
+        ("W3", "W1"),
+        ("W3", "Y"),
+        ("W4", "Y"),
+    ]
+)
+L_2 = ["T"]
+N_2 = ["A", "Y", "T", "W1", "W2", "W3", "W4"]
+treatment_2 = "A"
+outcome_2 = "Y"
 
 EXAMPLES.append(CausalGraphExample(G_2, treatment_2, outcome_2, L_2, N_2))
 
-OPTIMALS.append(set(['T', 'W2', 'W3', 'W4']))
-OPTIMALS_MINIMAL.append(set(['T', 'W2', 'W3']))
-OPTIMALS_MINIMUM.append(set(['T', 'W1']))
+OPTIMALS.append(set(["T", "W2", "W3", "W4"]))
+OPTIMALS_MINIMAL.append(set(["T", "W2", "W3"]))
+OPTIMALS_MINIMUM.append(set(["T", "W1"]))
 
-# Figure 5 of the paper
+# Figure 5 of the Biometrika paper
 
 G_3 = CausalGraph()
-G_3.add_edges_from([('A', 'Y'),
-                    ('Z1', 'A'),
-                    ('Z1', 'Z2'),
-                    ('U', 'Z2'),
-                    ('U', 'Y')])
+G_3.add_edges_from([("A", "Y"), ("Z1", "A"), ("Z1", "Z2"), ("U", "Z2"), ("U", "Y")])
 L_3 = []
-N_3 = ['A', 'Y', 'Z1', 'Z2']
-treatment_3 = 'A'
-outcome_3 = 'Y'
+N_3 = ["A", "Y", "Z1", "Z2"]
+treatment_3 = "A"
+outcome_3 = "Y"
 
 EXAMPLES.append(CausalGraphExample(G_3, treatment_3, outcome_3, L_3, N_3))
 
-OPTIMALS.append("Conditions to guarantee the existence of an optimal adjustment set are not satisfied")
+OPTIMALS.append(
+    "Conditions to guarantee the existence of an optimal adjustment set are not satisfied"
+)
 OPTIMALS_MINIMAL.append(set())
 OPTIMALS_MINIMUM.append(set())
 
-# Figure 6 of the paper
+# Figure 6 of the Biometrika paper
 
 G_4 = CausalGraph()
-G_4.add_edges_from([('T', 'A'),
-                    ('A', 'Y'),
-                    ('U', 'Y'),
-                    ('U', 'F')])
-L_4 = ['T']
-N_4 = ['A', 'Y', 'T', 'F']
-treatment_4 = 'A'
-outcome_4 = 'Y'
+G_4.add_edges_from([("T", "A"), ("A", "Y"), ("U", "Y"), ("U", "F")])
+L_4 = ["T"]
+N_4 = ["A", "Y", "T", "F"]
+treatment_4 = "A"
+outcome_4 = "Y"
 
 EXAMPLES.append(CausalGraphExample(G_4, treatment_4, outcome_4, L_4, N_4))
 
-OPTIMALS.append("Conditions to guarantee the existence of an optimal adjustment set are not satisfied")
-OPTIMALS_MINIMAL.append(set('T'))
-OPTIMALS_MINIMUM.append(set('T'))
+OPTIMALS.append(
+    "Conditions to guarantee the existence of an optimal adjustment set are not satisfied"
+)
+OPTIMALS_MINIMAL.append(set("T"))
+OPTIMALS_MINIMUM.append(set("T"))
 
 # Example with no observable adjustment set
 G_5 = CausalGraph()
-G_5.add_edges_from([('A', 'Y'),
-                    ('U', 'Y'),
-                    ('U', 'A')])
+G_5.add_edges_from([("A", "Y"), ("U", "Y"), ("U", "A")])
 L_5 = []
-N_5 = ['A', 'Y']
-treatment_5 = 'A'
-outcome_5 = 'Y'
+N_5 = ["A", "Y"]
+treatment_5 = "A"
+outcome_5 = "Y"
 
 EXAMPLES.append(CausalGraphExample(G_5, treatment_5, outcome_5, L_5, N_5))
 
 OPTIMALS.append("An adjustment set formed by observable variables does not exist")
-OPTIMALS_MINIMAL.append("An adjustment set formed by observable variables does not exist")
-OPTIMALS_MINIMUM.append("An adjustment set formed by observable variables does not exist")
+OPTIMALS_MINIMAL.append(
+    "An adjustment set formed by observable variables does not exist"
+)
+OPTIMALS_MINIMUM.append(
+    "An adjustment set formed by observable variables does not exist"
+)
+
+# Figures 2 and 3 of optimal minimum cost paper
+
+G_6 = CausalGraph()
+costs_6 = [
+    ("X", {"cost": 1}),
+    ("B", {"cost": 2}),
+    ("K", {"cost": 4}),
+    ("Q", {"cost": 1}),
+    ("R", {"cost": 1}),
+    ("T", {"cost": 1}),
+]
+L_6 = ["X"]
+N_6 = ["A", "Y", "X", "B", "K", "Q", "R", "M", "T", "F"]
+treatment_6 = "A"
+outcome_6 = "Y"
+G_6.add_nodes_from(costs_6)
+G_6.add_edges_from(
+    [
+        ("X", "A"),
+        ("A", "M"),
+        ("K", "A"),
+        ("B", "K"),
+        ("Q", "K"),
+        ("B", "R"),
+        ("Q", "T"),
+        ("R", "Y"),
+        ("M", "Y"),
+        ("T", "Y"),
+        ("U", "Y"),
+        ("U", "F"),
+    ]
+)
+
+EXAMPLES.append(CausalGraphExample(G_6, treatment_6, outcome_6, L_6, N_6))
+
+OPTIMALS.append(
+    "Conditions to guarantee the existence of an optimal adjustment set are not satisfied"
+)
+OPTIMALS_MINIMAL.append(set(["X", "T", "R"]))
+OPTIMALS_MINIMUM.append(set(["X", "K"]))
+OPTIMALS_MINCOST.append(set(["X", "T", "R"]))
--- tests/test_CausalGraph.py
+++ tests/test_CausalGraph.py
@@ -1,63 +1,83 @@
 import pytest
+
 from optimaladj.CausalGraph import ConditionException, NoAdjException
-from tests.examples import EXAMPLES, OPTIMALS, OPTIMALS_MINIMAL, OPTIMALS_MINIMUM
+from tests.examples import (EXAMPLES, OPTIMALS, OPTIMALS_MINCOST,
+                            OPTIMALS_MINIMAL, OPTIMALS_MINIMUM)
 
 
 @pytest.mark.parametrize("example, optimal_stored", zip(EXAMPLES[0:3], OPTIMALS[0:3]))
 def test_optimal(example, optimal_stored):
-    optimal = example.G.optimal_adj_set(treatment=example.treatment,
-                                        outcome=example.outcome,
-                                        L=example.L,
-                                        N=example.N)
+    optimal = example.G.optimal_adj_set(
+        treatment=example.treatment, outcome=example.outcome, L=example.L, N=example.N
+    )
     assert optimal == optimal_stored
 
 
 @pytest.mark.parametrize("example", EXAMPLES[3:5])
 def test_optimal_failure(example):
     with pytest.raises(ConditionException):
-        example.G.optimal_adj_set(treatment=example.treatment,
-                                  outcome=example.outcome,
-                                  L=example.L,
-                                  N=example.N)
+        example.G.optimal_adj_set(
+            treatment=example.treatment,
+            outcome=example.outcome,
+            L=example.L,
+            N=example.N,
+        )
 
 
-@pytest.mark.parametrize("example, optimal_minimal_stored", zip(EXAMPLES[0:5], OPTIMALS_MINIMAL[0:5]))
+@pytest.mark.parametrize(
+    "example, optimal_minimal_stored", zip(EXAMPLES[0:5], OPTIMALS_MINIMAL[0:5])
+)
 def test_optimal_minimal(example, optimal_minimal_stored):
-    optimal = example.G.optimal_minimal_adj_set(treatment=example.treatment,
-                                                outcome=example.outcome,
-                                                L=example.L,
-                                                N=example.N)
+    optimal = example.G.optimal_minimal_adj_set(
+        treatment=example.treatment, outcome=example.outcome, L=example.L, N=example.N
+    )
     assert optimal == optimal_minimal_stored
 
 
-@pytest.mark.parametrize("example, optimal_minimum_stored", zip(EXAMPLES[0:5], OPTIMALS_MINIMUM[0:5]))
+@pytest.mark.parametrize(
+    "example, optimal_minimum_stored", zip(EXAMPLES[0:5], OPTIMALS_MINIMUM[0:5])
+)
 def test_optimal_minimum(example, optimal_minimum_stored):
-    optimal = example.G.optimal_minimum_adj_set(treatment=example.treatment,
-                                                outcome=example.outcome,
-                                                L=example.L,
-                                                N=example.N)
+    optimal = example.G.optimal_minimum_adj_set(
+        treatment=example.treatment, outcome=example.outcome, L=example.L, N=example.N
+    )
     assert optimal == optimal_minimum_stored
 
 
 def test_no_adj_optimal(example=EXAMPLES[5]):
     with pytest.raises(NoAdjException):
-        example.G.optimal_adj_set(treatment=example.treatment,
-                                  outcome=example.outcome,
-                                  L=example.L,
-                                  N=example.N)
+        example.G.optimal_adj_set(
+            treatment=example.treatment,
+            outcome=example.outcome,
+            L=example.L,
+            N=example.N,
+        )
 
 
 def test_no_adj_minimal_optimal(example=EXAMPLES[5]):
     with pytest.raises(NoAdjException):
-        example.G.optimal_minimal_adj_set(treatment=example.treatment,
-                                          outcome=example.outcome,
-                                          L=example.L,
-                                          N=example.N)
+        example.G.optimal_minimal_adj_set(
+            treatment=example.treatment,
+            outcome=example.outcome,
+            L=example.L,
+            N=example.N,
+        )
 
 
 def test_no_adj_minimum_optimal(example=EXAMPLES[5]):
     with pytest.raises(NoAdjException):
-        example.G.optimal_minimum_adj_set(treatment=example.treatment,
-                                          outcome=example.outcome,
-                                          L=example.L,
-                                          N=example.N)
+        example.G.optimal_minimum_adj_set(
+            treatment=example.treatment,
+            outcome=example.outcome,
+            L=example.L,
+            N=example.N,
+        )
+
+
+def test_optimal_mincost(
+    example=EXAMPLES[6], optimal_mincost_stored=OPTIMALS_MINCOST[0]
+):
+    optimal = example.G.optimal_mincost_adj_set(
+        treatment=example.treatment, outcome=example.outcome, L=example.L, N=example.N
+    )
+    assert optimal == optimal_mincost_stored
